0d47edd6b435f03996ba67c1985de0d1ddc8d996
apandey16 <apande02@calpoly.edu>
2024-05-17 14:29:19 -0700

    some work done

---
+++ secret_agendas/src/components/header/style.tsx
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    // TODO: add fun header style
+
+    // TODO: add fun text style
+    
+});
+
+export default styles;
---
+++ secret_agendas/src/pages/home/home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../../components/header/header';
+
+ const Home: React.FC = () => {
+  return (
+    <main>
+      <Header/>
+    </main>
+  );
+}
+
+export default Home;
