bad1e2f51edd43f59ec2deaa2492615bc6249705
apandey16 <apande02@calpoly.edu>
2024-05-18 01:59:36 -0700

    added some comments

--- secret_agendas/src/components/TaskHandler/TaskHandler.tsx
+++ secret_agendas/src/components/TaskHandler/TaskHandler.tsx
@@ -16,7 +16,7 @@ const TaskHandler: React.FC = () => {
             navigate("/");
             Cookies.set("userId", generateUniqueId(), { expires: 1 });
             Cookies.set("tasksFinished", '0', { expires: 1 });
-            alert("You have failed your last task and your mission is over. Please click continue to start a new mission.");
+            alert("You have failed your last task and your mission is over.\nYou MUST take a shot before moving on.\nPlease click continue to start a new mission.");
             return;
         }
         const tasksFinished = parseInt(tasks || "0") - 1;
@@ -35,6 +35,7 @@ const TaskHandler: React.FC = () => {
 
     return (
         <header style={{ textAlign: 'center' }}>
+            {/* Route to new failure and success page to present the failure task or the success result */}
             <pre>{"Task Handler"}</pre>
 
             <button onClick={() => { handleFailure() }}>Task Failed</button>
