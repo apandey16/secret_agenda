bc3e301d034bfebbc12cfac652c3893b405327f7
apandey16 <apande02@calpoly.edu>
2024-05-18 23:42:26 -0700

    Revert "udpated tab name"
    This reverts commit 82260a3eb88034df543b23f814966cc541d10924.

--- secret_agendas/src/components/TaskHandler/punishments.tsx
+++ secret_agendas/src/components/TaskHandler/punishments.tsx
@@ -1,7 +1,4 @@
 export const punishments = {
     1: 'Take a shot',
     2: 'Swap your pants with your shirt till you complete the next task',
-    3: 'Do 10 pushups',
-    4: 'Do 10 situps',
-    5: "Let someone go through your phone for 2 minutes",
 }
